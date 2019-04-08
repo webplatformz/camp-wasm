27d15e787626fe331c9265c0adf7d52d35fd49da
r3dDoX <patrick@walther-online.ch>
2019-04-08 14:22:30 +0300

    use video capture

--- main.js
+++ main.js
@@ -12,10 +12,18 @@ imgElement.onload = function () {
     cv.threshold(mat, mat, 127, 255, 0);
     cv.findContours(mat, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
-    for (let i = 0; i < contours.size(); ++i) {
-        let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255));
-        cv.drawContours(mat, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-    }
+  for (let i = 0; i < contours.size(); ++i) {
+    let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255));
+    cv.drawContours(dest, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+  }
+  cv.imshow('canvasOutput', dest);
+  window.requestAnimationFrame(() => render(cap, src, dest));
+}
+
+videoInput.addEventListener('play', () => {
+  const src = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
+  const dest = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
+  const cap = new cv.VideoCapture(videoInput);
 
     cv.imshow('canvasOutput', mat);
     mat.delete();
