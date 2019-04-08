dd730827f4b94e36d91e5f19bd4d633a29e46d8d
Mario Egli <marioegli95@gmail.com>
2019-04-08 16:46:30 +0300

    More edge detection

--- render.js
+++ render.js
@@ -21,8 +21,16 @@ function findRectangle(input, output) {
   const contours = new cv.MatVector();
   const hierarchy = new cv.Mat();
 
-  cv.cvtColor(input, output, cv.COLOR_RGB2GRAY);
-  cv.threshold(output, output, 127, 255, 0);
+  cv.cvtColor(input, output, cv.COLOR_BGR2GRAY);
+
+  // cv.bilateralFilter(output, output, 5, 75, 75, cv.BORDER_DEFAULT);
+
+  cv.adaptiveThreshold(output, output, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 115, 4);
+  cv.medianBlur(output, output, 11)
+
+  cv.copyMakeBorder(output, output, 5, 5, 5, 5, cv.BORDER_CONSTANT, new cv.Scalar(0, 0, 0));
+  cv.Canny(output, output, 370, 160)
+
   cv.findContours(output, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
   for (let i = 0; i < contours.size(); ++i) {
@@ -32,8 +40,10 @@ function findRectangle(input, output) {
     let approx = new cv.Mat();
     cv.approxPolyDP(contours.get(i), approx, epsilon, true);
 
-    if (approx.rows === 4) {
+    if (approx.matSize === 4) {
       let boundRect = cv.boundingRect(contours.get(i));
+
+      console.log(approx)
 
       const topLeft = new cv.Point(boundRect.x, boundRect.y)
       const bottomRight = new cv.Point(boundRect.x + boundRect.width, boundRect.y + boundRect.height)
