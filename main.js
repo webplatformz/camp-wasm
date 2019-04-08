5260d7177ec14e0071bdbf9157e7f53b2369e89e
r3dDoX <patrick@walther-online.ch>
2019-04-08 17:31:53 +0300

    add basic style guide

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import { init, render } from './render';
+import {init, render} from './render';
 
 const videoInput = document.getElementById('videoInput');
 
 const videoDataLoaded = new Promise(resolve =>
-  videoInput.addEventListener('loadeddata', () => {
-    videoInput.width = videoInput.clientWidth;
-    videoInput.height = videoInput.clientHeight;
-    resolve();
-  })
+    videoInput.addEventListener('loadeddata', () => {
+        videoInput.width = videoInput.clientWidth;
+        videoInput.height = videoInput.clientHeight;
+        resolve();
+    }),
 );
 const openCvRuntimeLoaded = new Promise(resolve => cv.onRuntimeInitialized = resolve);
 
 Promise
-  .all([videoDataLoaded, openCvRuntimeLoaded])
-  .then(() => {
-    init(videoInput);
-    render();
-  });
+    .all([videoDataLoaded, openCvRuntimeLoaded])
+    .then(() => {
+        init(videoInput);
+        render();
+    });
 
 navigator.mediaDevices
-  .getUserMedia({ audio: false, video: { facingMode: ['environment', 'user'] } })
-  .then(stream => videoInput.srcObject = stream)
-  .catch(console.error);
+    .getUserMedia({audio: false, video: {facingMode: ['environment', 'user']}})
+    .then(stream => videoInput.srcObject = stream)
+    .catch(console.error);
--- render.js
+++ render.js
@@ -4,61 +4,63 @@ let dstMat;
 
 
 export function init(videoInput) {
-  videoCapture = new cv.VideoCapture(videoInput);
-  srcMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
-  dstMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
+    videoCapture = new cv.VideoCapture(videoInput);
+    srcMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
+    dstMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
 }
 
 export function render() {
-  videoCapture.read(srcMat);
+    videoCapture.read(srcMat);
 
-  findRectangle(srcMat, dstMat)
+    findRectangle(srcMat, dstMat);
 
-  cv.imshow('canvasOutput', srcMat);
-  window.requestAnimationFrame(render);
+    cv.imshow('canvasOutput', srcMat);
+    window.requestAnimationFrame(render);
 }
 
 function findRectangle(input, output) {
-  const contours = new cv.MatVector();
-  const hierarchy = new cv.Mat();
+    const contours = new cv.MatVector();
+    const hierarchy = new cv.Mat();
 
-  let biggestContour;
+    let biggestContour;
 
-  cv.cvtColor(input, output, cv.COLOR_BGR2GRAY);
+    cv.cvtColor(input, output, cv.COLOR_BGR2GRAY);
 
-  // cv.bilateralFilter(output, output, 5, 75, 75, cv.BORDER_DEFAULT);
+    // cv.bilateralFilter(output, output, 5, 75, 75, cv.BORDER_DEFAULT);
 
-  cv.adaptiveThreshold(output, output, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 115, 4);
-  cv.medianBlur(output, output, 11)
+    cv.adaptiveThreshold(output, output, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 115, 4);
+    cv.medianBlur(output, output, 11);
 
-  cv.copyMakeBorder(output, output, 5, 5, 5, 5, cv.BORDER_CONSTANT, new cv.Scalar(0, 0, 0));
-  cv.Canny(output, output, 370, 160)
+    cv.copyMakeBorder(output, output, 5, 5, 5, 5, cv.BORDER_CONSTANT, new cv.Scalar(0, 0, 0));
+    cv.Canny(output, output, 370, 160);
 
-  cv.findContours(output, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+    cv.findContours(output, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
-  for (let i = 0; i < contours.size(); ++i) {
+    for (let i = 0; i < contours.size(); ++i) {
 
-    let epsilon = 0.1 * cv.arcLength(contours.get(i), true);
-    let approx = new cv.Mat();
-    cv.approxPolyDP(contours.get(i), approx, epsilon, true);
+        let epsilon = 0.1 * cv.arcLength(contours.get(i), true);
+        let approx = new cv.Mat();
+        cv.approxPolyDP(contours.get(i), approx, epsilon, true);
 
-    if (approx.rows === 4) {
-      let area = cv.contourArea(contours.get(i))
+        if (approx.rows === 4) {
+            let area = cv.contourArea(contours.get(i));
 
-      if(biggestContour===undefined) {
-        biggestContour = contours.get(i)
-      } else if(area > cv.contourArea(biggestContour)) {
-        biggestContour = contours.get(i);
-      }
+            if (biggestContour === undefined) {
+                biggestContour = contours.get(i);
+            } else if (area > cv.contourArea(biggestContour)) {
+                biggestContour = contours.get(i);
+            }
+        }
+
+        approx.delete();
     }
-  }
 
-  let color = new cv.Scalar(0, 255, 0);
-  let boundRect = cv.boundingRect(biggestContour);
-  const topLeft = new cv.Point(boundRect.x, boundRect.y)
-  const bottomRight = new cv.Point(boundRect.x + boundRect.width, boundRect.y + boundRect.height)
-  cv.rectangle(input, topLeft, bottomRight, color, 1, 16);
+    let color = new cv.Scalar(0, 255, 0);
+    let boundRect = cv.boundingRect(biggestContour);
+    const topLeft = new cv.Point(boundRect.x, boundRect.y);
+    const bottomRight = new cv.Point(boundRect.x + boundRect.width, boundRect.y + boundRect.height);
+    cv.rectangle(input, topLeft, bottomRight, color, 1, 16);
 
-  contours.delete();
-  hierarchy.delete();
+    contours.delete();
+    hierarchy.delete();
 }
