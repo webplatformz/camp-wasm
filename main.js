1e54ec7c8f9d589d95920995021cfd2e2ffc1e50
r3dDoX <patrick@walther-online.ch>
2019-04-08 11:26:45 +0300

    move js to own module and add example pics

---
+++ main.js
@@ -0,0 +1,11 @@
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+inputElement.addEventListener('change', (e) => {
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+
+imgElement.onload = function() {
+  let mat = cv.imread(imgElement);
+  cv.imshow('canvasOutput', mat);
+  mat.delete();
+};
