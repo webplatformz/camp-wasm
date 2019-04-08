3b8e5e833d51c5d0ea09e218156692d72689f5de
r3dDoX <patrick@walther-online.ch>
2019-04-08 14:32:07 +0300

    fix partial commit

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-let imgElement = document.getElementById('imageSrc');
-let inputElement = document.getElementById('fileInput');
-inputElement.addEventListener('change', (e) => {
-    imgElement.src = URL.createObjectURL(e.target.files[0]);
-}, false);
+import { render } from './render.js';
 
-imgElement.onload = function () {
-    let mat = cv.imread(imgElement);
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    cv.cvtColor(mat, mat, cv.COLOR_RGB2GRAY);
-    cv.threshold(mat, mat, 127, 255, 0);
-    cv.findContours(mat, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+const videoInput = document.getElementById('videoInput');
 
-  for (let i = 0; i < contours.size(); ++i) {
-    let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255));
-    cv.drawContours(dest, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-  }
-  cv.imshow('canvasOutput', dest);
-  window.requestAnimationFrame(() => render(cap, src, dest));
-}
+cv.onRuntimeInitialized = () => {
+  const srcMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
+  const dstMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
+  const videoCapture = new cv.VideoCapture(videoInput);
+
+  render(videoCapture, srcMat, dstMat);
+};
 
 navigator.mediaDevices.getUserMedia({ audio: false, video: true })
   .then(stream => videoInput.srcObject = stream)
