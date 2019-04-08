8f5b3b68294d2117a47aa9d40b75b690106f0260
Mario Egli <marioegli95@gmail.com>
2019-04-08 17:13:46 +0300

    Show biggest contour only

--- render.js
+++ render.js
@@ -2,6 +2,7 @@ let videoCapture;
 let srcMat;
 let dstMat;
 
+
 export function init(videoInput) {
   videoCapture = new cv.VideoCapture(videoInput);
   srcMat = new cv.Mat(videoInput.height, videoInput.width, cv.CV_8UC4);
@@ -13,13 +14,15 @@ export function render() {
 
   findRectangle(srcMat, dstMat)
 
-  cv.imshow('canvasOutput', dstMat);
+  cv.imshow('canvasOutput', srcMat);
   window.requestAnimationFrame(render);
 }
 
 function findRectangle(input, output) {
   const contours = new cv.MatVector();
   const hierarchy = new cv.Mat();
+
+  let biggestContour;
 
   cv.cvtColor(input, output, cv.COLOR_BGR2GRAY);
 
@@ -34,22 +37,27 @@ function findRectangle(input, output) {
   cv.findContours(output, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
   for (let i = 0; i < contours.size(); ++i) {
-    let color = new cv.Scalar(255, 165, 0);
 
     let epsilon = 0.1 * cv.arcLength(contours.get(i), true);
     let approx = new cv.Mat();
     cv.approxPolyDP(contours.get(i), approx, epsilon, true);
 
-    if (approx.matSize === 4) {
-      let boundRect = cv.boundingRect(contours.get(i));
+    if (approx.rows === 4) {
+      let area = cv.contourArea(contours.get(i))
 
-      console.log(approx)
-
-      const topLeft = new cv.Point(boundRect.x, boundRect.y)
-      const bottomRight = new cv.Point(boundRect.x + boundRect.width, boundRect.y + boundRect.height)
-      cv.rectangle(output, topLeft, bottomRight, color, 1, 16);
+      if(biggestContour===undefined) {
+        biggestContour = contours.get(i)
+      } else if(area > cv.contourArea(biggestContour)) {
+        biggestContour = contours.get(i);
+      }
     }
   }
+
+  let color = new cv.Scalar(0, 255, 0);
+  let boundRect = cv.boundingRect(biggestContour);
+  const topLeft = new cv.Point(boundRect.x, boundRect.y)
+  const bottomRight = new cv.Point(boundRect.x + boundRect.width, boundRect.y + boundRect.height)
+  cv.rectangle(input, topLeft, bottomRight, color, 1, 16);
 
   contours.delete();
   hierarchy.delete();
