b1a29ed5476780cbe2abe202b5f14c39937ac2fa
Simon Müller <SimonMueller@users.noreply.github.com>
2019-04-09 15:15:26 +0200

    test new canny params

--- filter.worker.js
+++ filter.worker.js
@@ -14,7 +14,7 @@ addEventListener('message', function handleMessage({data}) {
 
     cv.medianBlur(imgMat, imgMat, 7);
 
-    cv.Canny(imgMat, imgMat, 175, 60);
+    cv.Canny(imgMat, imgMat, 80, 100);
 
     cv.findContours(imgMat, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE);
 
