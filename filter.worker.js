33254eb41fec48898da69ff7e557f8c7ec99781b
r3dDoX <patrick@walther-online.ch>
2019-04-10 10:12:26 +0300

    use points instead of explicit rectangle to draw on canvas

--- filter.worker.js
+++ filter.worker.js
@@ -4,51 +4,16 @@ cv.onRuntimeInitialized = () => postMessage({type: 'RUNTIME_INITIALIZED'});
 
 addEventListener('message', function handleMessage({data}) {
     const imgMat = cv.matFromImageData(data);
-    const contours = new cv.MatVector();
-    const hierarchy = new cv.Mat();
-
-    let highestFillRatio = 0;
-    let bestMatchingRectangle;
-
-    cv.cvtColor(imgMat, imgMat, cv.COLOR_BGR2GRAY);
-
-    cv.medianBlur(imgMat, imgMat, 7);
-
-    cv.Canny(imgMat, imgMat, 80, 190);
-
-    cv.findContours(imgMat, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE);
-
-    for (let i = 0; i < contours.size(); ++i) {
-        let currentContour = contours.get(i);
-        let contourBoundingRect = cv.boundingRect(currentContour);
-        let minBoundingRectWidth = imgMat.cols * 0.5;
-        let minBoundingRectHeight = imgMat.rows * 0.5;
-
-        if ( contourBoundingRect.width > minBoundingRectWidth && contourBoundingRect.height > minBoundingRectHeight) {
-            let boundingArea = contourBoundingRect.width * contourBoundingRect.height;
-            let contourArea = cv.contourArea(currentContour);
-            let fillRatio = contourArea / boundingArea;
-
-            if (highestFillRatio < fillRatio) {
-                highestFillRatio = fillRatio;
-                bestMatchingRectangle = contourBoundingRect;
-            }
-        }
-    }
-
-    postFrame(imgMat, bestMatchingRectangle);
-
-    contours.delete();
-    hierarchy.delete();
+    postFrame(imgMat, calculateBoundingRectPoints(imgMat));
     imgMat.delete();
 });
 
-function postFrame(imgMat, boundRect) {
+function postFrame(imgMat, points) {
     const imageData = convertToImageData(imgMat);
     postMessage({
         type: 'FRAME',
-        boundRect,
         imageData,
+        points,
     }, [imageData.data.buffer]);
 }
 
@@ -59,4 +24,44 @@ function convertToImageData(imgMat) {
     const imageData = new ImageData(new Uint8ClampedArray(dst.data, dst.cols, dst.rows), dst.cols);
     dst.delete();
     return imageData;
+}
+
+function hasMinSize(contourBoundingRect, minBoundingRectWidth, minBoundingRectHeight) {
+    return contourBoundingRect.size.width > minBoundingRectWidth
+        && contourBoundingRect.size.height > minBoundingRectHeight;
+}
+
+function calculateBoundingRectPoints(imgMat) {
+    const contours = new cv.MatVector();
+    const hierarchy = new cv.Mat();
+
+    let highestFillRatio = 0;
+    let points;
+
+    cv.cvtColor(imgMat, imgMat, cv.COLOR_BGR2GRAY);
+    cv.medianBlur(imgMat, imgMat, 7);
+    cv.Canny(imgMat, imgMat, 80, 190);
+    cv.findContours(imgMat, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE);
+
+    for (let i = 0; i < contours.size(); ++i) {
+        let currentContour = contours.get(i);
+        let contourBoundingRect = cv.minAreaRect(currentContour);
+        let minBoundingRectWidth = imgMat.cols * 0.5;
+        let minBoundingRectHeight = imgMat.rows * 0.5;
+
+        if (hasMinSize(contourBoundingRect, minBoundingRectWidth, minBoundingRectHeight)) {
+            let boundingArea = contourBoundingRect.size.width * contourBoundingRect.size.height;
+            let contourArea = cv.contourArea(currentContour);
+            let fillRatio = contourArea / boundingArea;
+
+            if (highestFillRatio < fillRatio) {
+                highestFillRatio = fillRatio;
+                points = cv.RotatedRect.points(contourBoundingRect);
+            }
+        }
+    }
+    contours.delete();
+    hierarchy.delete();
+
+    return points;
 }
