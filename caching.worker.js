6fdd8a95ff8f18190d6b30c485e883251e3df3a3
Mario Egli <marioegli95@gmail.com>
2019-04-12 13:10:39 +0300

    Remove console logs

--- caching.worker.js
+++ caching.worker.js
@@ -6,10 +6,8 @@ let filesToCache = [
     '/filter.worker.js',
 ];
 self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Install');
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         }),
     );
@@ -22,7 +20,6 @@ self.addEventListener('fetch', event => {
         caches.open(cacheName).then(cache => {
             return cache.match(event.request).then(response => {
                 return response || fetch(event.request).then(response => {
-                    console.log("caching file: " + event.request.url);
                     cache.put(event.request.url, response.clone());
                     return response;
                 });
