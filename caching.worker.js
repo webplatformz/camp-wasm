e0cc5164b7e3ddb4f381f616c997bc339746f71f
Mario Egli <marioegli95@gmail.com>
2019-04-12 11:31:00 +0300

    Add more logging

--- caching.worker.js
+++ caching.worker.js
@@ -5,22 +5,23 @@ let filesToCache = [
     'opencv/opencv.js',
     'filter.worker.js',
 ];
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
-        })
+        }),
     );
 });
-self.addEventListener('activate',  event => {
+self.addEventListener('activate', event => {
     event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request, {ignoreSearch:true}).then(response => {
+        caches.match(event.request, {ignoreSearch: true}).then(response => {
+            console.log('return cached file: ' + event.request);
             return response || fetch(event.request);
-        })
+        }),
     );
 });
